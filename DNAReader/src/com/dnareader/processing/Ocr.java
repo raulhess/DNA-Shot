1074e50018c4061500fb62d45b3fba26a214e92b
raulhess <raul.hess@gmail.com>
2014-10-22 16:40:56 -0200

    [DNAReader] Added missing files

---
+++ DNAReader/src/com/dnareader/processing/Ocr.java
@@ -0,0 +1,185 @@
+package com.dnareader.processing;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.os.Environment;
+import android.util.Log;
+
+import com.dnareader.activities.MainActivity;
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+
+public class Ocr {
+	
+	
+	
+	public static final String DATA_PATH = Environment.getExternalStorageDirectory().toString() + "/dnareader/";
+	
+	// You should have the trained data file in assets folder
+	// You can get them at:
+	// http://code.google.com/p/tesseract-ocr/downloads/list
+	public static final String lang = "eng";
+
+	private String TAG;
+
+
+	protected String _path;
+	protected boolean _taken;
+	private Context context;
+	
+	public Ocr(Context context) {
+		this.context = context;
+		TAG = MainActivity.TAG;
+		init();
+	}
+	
+
+	private void init(){
+
+
+		String[] paths = new String[] { DATA_PATH, DATA_PATH + "tessdata/" };
+
+		for (String path : paths) {
+			File dir = new File(path);
+			if (!dir.exists()) {
+				if (!dir.mkdirs()) {
+					Log.v(TAG, "ERROR: Creation of directory " + path + " on sdcard failed");
+					return;
+				} else {
+					Log.v(TAG, "Created directory " + path + " on sdcard");
+				}
+			}
+
+		}
+		
+		// lang.traineddata file with the app (in assets folder)
+		// You can get them at:
+		// http://code.google.com/p/tesseract-ocr/downloads/list
+		// This area needs work and optimization
+		if (!(new File(DATA_PATH + "tessdata/" + lang + ".traineddata")).exists()) {
+			try {
+
+				AssetManager assetManager = context.getAssets();
+				InputStream in = assetManager.open("tessdata/" + lang + ".traineddata");
+				//GZIPInputStream gin = new GZIPInputStream(in);
+				OutputStream out = new FileOutputStream(DATA_PATH
+						+ "tessdata/" + lang + ".traineddata");
+
+				// Transfer bytes from in to out
+				byte[] buf = new byte[1024];
+				int len;
+				//while ((lenf = gin.read(buff)) > 0) {
+				while ((len = in.read(buf)) > 0) {
+					out.write(buf, 0, len);
+				}
+				in.close();
+				//gin.close();
+				out.close();
+				
+				Log.v(TAG, "Copied " + lang + " traineddata");
+			} catch (IOException e) {
+				Log.e(TAG, "Was unable to copy " + lang + " traineddata " + e.toString());
+			}
+		}
+
+
+		_path = DATA_PATH + "/ocr.jpg";
+	}
+	
+	public String doOcr(byte[] image){
+
+			final long startTime = System.currentTimeMillis();
+			_taken = true;
+
+			BitmapFactory.Options options = new BitmapFactory.Options();
+			options.inSampleSize = 4;
+
+			Bitmap bitmap = BitmapFactory.decodeByteArray(image, 0, image.length,options);
+
+			try {
+				ExifInterface exif = new ExifInterface(_path);
+				int exifOrientation = exif.getAttributeInt(
+						ExifInterface.TAG_ORIENTATION,
+						ExifInterface.ORIENTATION_NORMAL);
+
+				Log.v(TAG, "Orient: " + exifOrientation);
+
+				int rotate = 0;
+
+				switch (exifOrientation) {
+				case ExifInterface.ORIENTATION_ROTATE_90:
+					rotate = 90;
+					break;
+				case ExifInterface.ORIENTATION_ROTATE_180:
+					rotate = 180;
+					break;
+				case ExifInterface.ORIENTATION_ROTATE_270:
+					rotate = 270;
+					break;
+				}
+
+				Log.v(TAG, "Rotation: " + rotate);
+
+				if (rotate != 0) {
+
+					// Getting width & height of the given image.
+					int w = bitmap.getWidth();
+					int h = bitmap.getHeight();
+
+					// Setting pre rotate
+					Matrix mtx = new Matrix();
+					mtx.preRotate(rotate);
+
+					// Rotating Bitmap
+					bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false);
+				}
+
+				// Convert to ARGB_8888, required by tess
+				bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+
+			} catch (IOException e) {
+				Log.e(TAG, "Couldn't correct orientation: " + e.toString());
+			}
+
+			// _image.setImageBitmap( bitmap );
+			
+			Log.v(TAG, "Before baseApi");
+
+			TessBaseAPI baseApi = new TessBaseAPI();
+			baseApi.setDebug(true);
+			baseApi.init(DATA_PATH, lang);
+			baseApi.setImage(bitmap);
+			
+			String recognizedText = baseApi.getUTF8Text();
+			
+			baseApi.end();
+
+			// You now have the text in recognizedText var, you can do anything with it.
+			// We will display a stripped out trimmed alpha-numeric version of it (if lang is eng)
+			// so that garbage doesn't make it to the display.
+
+			Log.v(TAG, "OCRED TEXT: " + recognizedText);
+
+			
+			recognizedText = recognizedText.replaceAll("[^a-zA-Z0-9]+", " ");
+						
+		
+
+			final long endTime = System.currentTimeMillis();
+			Log.v(TAG, "Tempo onPhotoTaken():" + (endTime-startTime)/1000.0);
+			
+			return recognizedText;		
+
+	}
+	
+}
